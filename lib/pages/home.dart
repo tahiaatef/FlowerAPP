e78c7e6e0c9ee029c59f21908e31be89fe9a21ef
tya <99808858+tahiaatef@users.noreply.github.com>
2025-01-15 17:09:04 +0300

    add drawer in home page

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,18 +1,66 @@
-// ignore_for_file: sort_child_properties_last
-
 import 'package:flutter/material.dart';
 import 'package:flutter_application_6/shared/colors.dart';
 
 class Home extends StatelessWidget {
   const Home({super.key});
-
-  get padding => null;
-
   @override
   Widget build(BuildContext context) {
     return SafeArea(
         child: Scaffold(
+      drawer: Drawer(
+        child: Column(children: [
+          UserAccountsDrawerHeader(
+              decoration: BoxDecoration(
+                  image: DecorationImage(
+                      image: AssetImage("assets/imgs/drawerbg.jpg"),
+                      fit: BoxFit.cover)),
+              currentAccountPicture: CircleAvatar(
+                radius: 55,
+                backgroundImage:
+                    AssetImage("assets/imgs/photo_2022-03-07_18-04-47.jpg"),
+              ),
+              accountName: Text("Sahil"),
+              accountEmail: Text("sahil@.com")),
+          ListTile(
+              title: Text("Home" , style: TextStyle(color:Color.fromARGB(255, 80, 40, 7),fontWeight: FontWeight.bold),), leading: Icon(Icons.home), onTap: () {}),
+          ListTile(
+              title: Text("My products",style: TextStyle(color:Color.fromARGB(255, 80, 40, 7),fontWeight: FontWeight.bold),),
+              leading: Icon(Icons.shopping_basket_rounded),
+              onTap: () {}),
+          ListTile(
+              title: Text("About",style: TextStyle(color:Color.fromARGB(255, 80, 40, 7),fontWeight: FontWeight.bold),),
+              leading: Icon(Icons.help_center),
+              onTap: () {}),
+          ListTile(
+              title: Text("Logout",style: TextStyle(color:Color.fromARGB(255, 80, 40, 7),fontWeight: FontWeight.bold),),
+              leading: Icon(Icons.exit_to_app),
+              onTap: () {}),
+          Container(
+              padding: EdgeInsets.only(top: 300),
+              child: Title(
+                color: Colors.grey,
+                child: Text(
+                  "Developed by Tahia Atef © 2024",
+                  style: TextStyle(fontSize: 16 , fontWeight: FontWeight.bold , color: Color.fromARGB(255, 80, 40, 7)),
+                ),
+              ))
+        ]),
+      ),
       appBar: AppBar(
+        leading: Builder(
+          builder: (context) {
+            return IconButton(
+              icon: Icon(
+                Icons.person,
+                color: const Color.fromARGB(255, 255, 255, 255),
+                size: 30,
+              ),
+              onPressed: () {
+                Scaffold.of(context).openDrawer();
+              },
+            );
+          },
+        ),
         backgroundColor: appbarGreen,
         title: Text(
           "Home",
@@ -23,14 +71,17 @@ class Home extends StatelessWidget {
             children: [
               Stack(
                 children: [
-                  IconButton(onPressed: () {}, 
-                  icon: Icon(Icons.shopping_basket_rounded),
-                  color: Colors.white,
+                  IconButton(
+                    onPressed: () {},
+                    icon: Icon(Icons.shopping_basket_rounded),
+                    color: Colors.white,
                   ),
-                    Container(
+                  Container(
                       child: Text(
                         "8",
-                        style: TextStyle(fontSize: 14,color: const Color.fromARGB(255, 128, 73, 18)),
+                        style: TextStyle(
+                            fontSize: 14,
+                            color: const Color.fromARGB(255, 128, 73, 18)),
                       ),
                       padding: EdgeInsets.all(5),
                       decoration: BoxDecoration(
@@ -40,8 +91,8 @@ class Home extends StatelessWidget {
               ),
               Padding(
                 padding: const EdgeInsets.only(right: 12),
-                child: Text("\$128" , style: TextStyle(color: Colors.white,fontSize:18)),
-                
+                child: Text("\$ 128",
+                    style: TextStyle(color: Colors.white, fontSize: 18)),
               )
             ],
           )
--- lib/shared/colors.dart
+++ lib/shared/colors.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: constant_identifier_names
+
 import 'package:flutter/material.dart';
 const BTNpink = Color.fromARGB(255, 241, 39, 100);
 const BTNgreen = Color.fromARGB(255, 179, 122, 73);

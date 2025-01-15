82a2b31308cf6584120e42bf4aa62632fbd42a6d
tya <99808858+tahiaatef@users.noreply.github.com>
2025-01-15 09:15:13 +0300

    create log in page

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -1,5 +1,8 @@
+// ignore_for_file: deprecated_member_use
+
 import 'package:flutter/material.dart';
-import 'package:flutter_application_6/shared/custom_textfiled.dart';
+import 'package:flutter_application_6/shared/colors.dart';
+import 'package:flutter_application_6/shared/constant.dart';
 
 class Login extends StatelessWidget {
   const Login({super.key});
@@ -8,27 +11,63 @@ class Login extends StatelessWidget {
   Widget build(BuildContext context) {
     return SafeArea(
         child: Scaffold(
+          backgroundColor: Color.from(alpha: 0, red: 0, green: 0, blue: 0),
       body: Center(
         child: Padding(
           padding: const EdgeInsets.all(33.0),
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
               SizedBox(
                 height: 64,
               ),
-              Mytextfiled(
-                textinputtype: TextInputType.emailAddress,
-                obsuretext: false,
-                hinttext: "Enter Your Email",
+              TextField(
+                keyboardType: TextInputType.emailAddress,
+                obscureText: false,
+                decoration: consttextfile.copyWith(
+                  hintText: "Enter your Email",
+                ),
               ),
               SizedBox(
-                height: 20,
+                height: 33,
               ),
-              Mytextfiled(
-                textinputtype: TextInputType.visiblePassword,
-                obsuretext: true,
-                hinttext: "Enter Your Password",
+              TextField(
+                keyboardType: TextInputType.visiblePassword,
+                obscureText: true,
+                decoration: consttextfile.copyWith(
+                  hintText: "Enter your Password",
+                ),
               ),
+              SizedBox(
+                height: 33,
+              ),
+              ElevatedButton(
+                onPressed: () {},
+                style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all(BTNgreen),
+                  padding: MaterialStateProperty.all(EdgeInsets.all(12)),
+                  shape: MaterialStateProperty.all(RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8))),
+                ),
+                child: Text(
+                  "sign in",
+                  style: TextStyle(fontSize: 19, color: Colors.white),
+                ),
+              ),
+              SizedBox(
+                height: 33,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text("Don't have an account?", style: TextStyle(fontSize: 19)),
+                  TextButton(
+                    onPressed: () {},
+                    child:
+                        Text('Sign Up', style: TextStyle(color: Colors.black,fontSize: 18 , fontWeight: FontWeight.bold)),
+                  )
+                ],
+              )
             ],
           ),
         ),
